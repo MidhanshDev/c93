8be2eb91fc16c8acadbda4e73e68c4082254959c
MidhanshDev <68544626+MidhanshDev@users.noreply.github.com>
2021-04-21 10:47:32 +0530

    Add files via upload

---
+++ screens/ReportScreen.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Image,
+} from "react-native";
+
+export default class ReportScreen extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return (
+      <View>
+        <Text>Report Screen</Text>
+      </View>
+    );
+  }
+}
---
+++ screens/SettingScreen.js
@@ -0,0 +1,21 @@
+import * as React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  FlatList,
+} from "react-native";
+export default class SettingScreen extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return (
+      <View>
+        <Text>Setting Screen</Text>
+      </View>
+    );
+  }
+}
